afb2b78882eb35592d9bcec01802572cbf09d1ba
HeyCat-svg <55990492+HeyCat-svg@users.noreply.github.com>
2019-10-14 21:16:52 +0800

    Add files via upload
    A website of maze

---
+++ maze/README.md
@@ -0,0 +1,28 @@
+# A SIMPLE MAZE IN HTML
+## The overview of widget
+<font face="Times New Roman" size="5" color="#000000">
+     &emsp;There is only a widget in this website which contains a maze,two input components and a button. <br> 
+     &emsp;These two input components can adjust the scale of the maze. User can input the width and height of the maze or do nothing which means maze will use the default setting of the scale of the maze.
+</font>
+## How to build a website(Code in html)
+<font face="Times New Roman" size="5" color="#000000">
+     &emsp;All the JavasCript code is from the file "maze.js". The web site just realizes the most basic function. There is actually no UI at all. 
+     All the objects are crowed at the left-top site of the page.
+     <br>
+     &emsp;The input component is set by <code>< input type="text" id="width" value="120"></code> and <code>< input type="text" id="height" value="80"></code> which set the default scale of the maze as 120*80.
+     &emsp;When the button is clicked, the page will call the function called <code>init(width, height)</code> and the function will reset the maze and change the scale  of the maze.
+</font>
+## How to run the maze(Code in JavaScript)
+<font face="Times New Roman" size="5" color="#000000">
+     &emsp;Running the maze can be divided into several steps.
+     <br>
+     1. A canvas should be created to hold the maze. The size of the canvas is decided by the width and heigth entered by the user or the default setting.Its color is set as "#00FF00" as default. The canvas is created by the function <code>createCanvas</code> in maze.js.
+     2. Now is time to create maze. Maze is created by DFS algorithm which means there's only one way connecting any two points in this maze. The maze is created by the function <code>createMaze()</code> in maze.js.
+     3. When the maze is created. User should decide the starting point and the ending point by clicking the black part in the maze. Then point clicked will change its color to yellow. Function <code>getCursorPos()</code> is responsible to this part.
+     4. After the starting point and the ending point is decied, the page starts to search the way from starting point to ending point using BFS algorithm too. When the program meets ramp in the searching way, it will choose the direction which leads to the shortest distance. But this strategy is sloppy because the shortest distance doesn't mean the right path. At one time, I try to sove the maze by BFS algorithm, but this strategy seems to be much more slower because it traverses more than a half of the maze. This part is completed by the founction <code>creatMaze()</code>in maze.js. 
+     5. Of course, all the painting work is compoleted by <code>drawMaze()</code>, the function which traverses the whole canvas from times to times, wasting much of time.
+</font>
+## How to improve the efficiency
+<font face="Times New Roman" size="5" color="#000000">
+     &emsp;The algorithm given by teacher is BFS, which is used to generate the maze and solve the maze. But the disadvantage of the algorithm is when slover chooses a wrong path, it will take solver many time to trace back to the previous ramp. Besides, <code>drawMaze()</code> also waste much time to traverse the whole canvas. So I try using BFS, which is much stable than DFS. But the efficiency doesn't change a lot. Actually, I don't know how to improve the efficiency in this case even if I searched some related information on the Internet.
+</font>
---
+++ maze/maze.js
@@ -0,0 +1,290 @@
+var ctx, wid, hei, cols, rows, maze, stack = [], start = {x:-1, y:-1}, end = {x:-1, y:-1}, grid = 8, padding = 16, s, density=0.5;
+function drawMaze() {
+    for( var i = 0; i < cols; i++ ) {
+        for( var j = 0; j < rows; j++ ) {
+            switch( maze[i][j] ) {
+                case 0: ctx.fillStyle = "black"; break;
+                case 1: ctx.fillStyle = "gray"; break;
+                case 2: ctx.fillStyle = "red"; break;
+                case 3: ctx.fillStyle = "yellow"; break;
+                case 4: ctx.fillStyle = "#500000"; break;
+                case 8: ctx.fillStyle = "blue"; break;
+            }
+            ctx.fillRect( grid * i, grid * j, grid, grid  );
+        }
+    }
+}
+
+function drawBlock(sx, sy, a) {
+    switch( a ) {
+        case 0: ctx.fillStyle = "black"; break;
+        case 1: ctx.fillStyle = "gray"; break;
+        case 2: ctx.fillStyle = "red"; break;
+        case 3: ctx.fillStyle = "yellow"; break;
+        case 4: ctx.fillStyle = "#500000"; break;
+        case 8: ctx.fillStyle = "blue"; break;
+    }
+    ctx.fillRect( grid * sx, grid * sy, grid, grid  );
+}
+
+function getFNeighbours( sx, sy, a ) {
+    var n = [];
+    if( sx - 1 > 0 && maze[sx - 1][sy] % 8 == a ) {
+        n.push( { x:sx - 1, y:sy } );
+    }
+    if( sx + 1 < cols - 1 && maze[sx + 1][sy] % 8 == a ) {
+        n.push( { x:sx + 1, y:sy } );
+    }
+    if( sy - 1 > 0 && maze[sx][sy - 1] % 8 == a ) {
+        n.push( { x:sx, y:sy - 1 } );
+    }
+    if( sy + 1 < rows - 1 && maze[sx][sy + 1] % 8 == a ) {
+        n.push( { x:sx, y:sy + 1 } );
+    }
+    return n;
+}
+function solveMaze1() {
+    if( start.x == end.x && start.y == end.y ) {
+        for( var i = 0; i < cols; i++ ) {
+            for( var j = 0; j < rows; j++ ) {
+                switch( maze[i][j] ) {
+                    case 2: maze[i][j] = 3; break;
+                    case 4: maze[i][j] = 0; break;
+                }
+            }
+        }
+        drawMaze();
+        return;
+    }
+    var neighbours = getFNeighbours( start.x, start.y, 0 );
+    if( neighbours.length ) {
+        stack.push( start );
+        start = neighbours[0];
+        maze[start.x][start.y] = 2;
+    } else {
+        maze[start.x][start.y] = 4;
+        start = stack.pop();
+    }
+ 
+    drawMaze();
+    requestAnimationFrame( solveMaze1 );
+}
+function getCursorPos( event ) {
+    var rect = this.getBoundingClientRect();
+    var x = Math.floor( ( event.clientX - rect.left ) / grid / s), 
+        y = Math.floor( ( event.clientY - rect.top  ) / grid / s);
+    if( maze[x][y] ) return;
+    if( start.x == -1 ) {
+        start = { x: x, y: y };
+        maze[start.x][start.y] = 8;
+        drawMaze();
+    } else {
+        end = { x: x, y: y };
+        maze[end.x][end.y] = 8;
+        solveMaze1();
+    }
+}
+function getNeighbours( sx, sy, a ) {
+    var n = [];
+    if( sx - 1 > 0 && maze[sx - 1][sy] == a && sx - 2 > 0 && maze[sx - 2][sy] == a ) {
+        n.push( { x:sx - 1, y:sy } ); n.push( { x:sx - 2, y:sy } );
+    }
+    if( sx + 1 < cols - 1 && maze[sx + 1][sy] == a && sx + 2 < cols - 1 && maze[sx + 2][sy] == a ) {
+        n.push( { x:sx + 1, y:sy } ); n.push( { x:sx + 2, y:sy } );
+    }
+    if( sy - 1 > 0 && maze[sx][sy - 1] == a && sy - 2 > 0 && maze[sx][sy - 2] == a ) {
+        n.push( { x:sx, y:sy - 1 } ); n.push( { x:sx, y:sy - 2 } );
+    }
+    if( sy + 1 < rows - 1 && maze[sx][sy + 1] == a && sy + 2 < rows - 1 && maze[sx][sy + 2] == a ) {
+        n.push( { x:sx, y:sy + 1 } ); n.push( { x:sx, y:sy + 2 } );
+    }
+    return n;
+}
+function createArray( c, r ) {
+    var m = new Array( c );
+    for( var i = 0; i < c; i++ ) {
+        m[i] = new Array( r );
+        for( var j = 0; j < r; j++ ) {
+            m[i][j] = 1;
+        }
+    }
+    return m;
+}
+function createMaze1() {
+    var neighbours = getNeighbours( start.x, start.y, 1 ), l;
+    if( neighbours.length < 1 ) {
+        if( stack.length < 1 ) {
+            drawMaze(); stack = [];
+            start.x = start.y = -1;
+            document.getElementById( "canvas" ).addEventListener( "mousedown", getCursorPos, false );
+            document.getElementById("btnCreateMaze").removeAttribute("disabled");
+
+            return;
+        }
+        start = stack.pop();
+    } else {
+        var i = 2 * Math.floor( Math.random() * ( neighbours.length / 2 ) )
+        l = neighbours[i]; 
+        maze[l.x][l.y] = 0;
+
+        l = neighbours[i + 1]; 
+        maze[l.x][l.y] = 0;
+
+        start = l
+        stack.push( start )
+    }
+    drawMaze();
+    requestAnimationFrame( createMaze1 );
+}
+
+function createMaze1NonAni() {
+
+    while(true) {
+
+        var neighbours = getNeighbours( start.x, start.y, 1 ), l;
+        if( neighbours.length < 1 ) {
+            if( stack.length < 1 ) {
+                drawMaze(); stack = [];
+                start.x = start.y = -1;
+                document.getElementById( "canvas" ).addEventListener( "mousedown", getCursorPos, false );
+                document.getElementById("btnCreateMaze").removeAttribute("disabled");
+    
+                return;
+            }
+            start = stack.pop();
+        } else {
+            var i = 2 * Math.floor( Math.random() * ( neighbours.length / 2 ) )
+            l = neighbours[i]; 
+            maze[l.x][l.y] = 0;
+    
+            l = neighbours[i + 1]; 
+            maze[l.x][l.y] = 0;
+    
+            start = l
+            stack.push( start )
+        }    
+    }
+    document.getElementById("btnCreateMaze").removeAttribute("disabled");
+}
+function createMaze2() {
+
+    var r = Math.random();
+
+    maze[start.x][start.y] = r < density ? 0 : 1;
+    
+    drawMaze();
+
+    if(start.x == (cols - 1) && start.y == (rows - 1)){
+
+        document.getElementById("btnCreateMaze").removeAttribute("disabled");
+        return;
+    }
+
+    start.x = start.x + 1;
+    if(start.x == cols){
+        start.x = 0;
+        start.y = start.y + 1;
+    }
+
+    requestAnimationFrame(createMaze2);
+}
+
+function createMaze2NonAni() {
+
+    for(var i = 0; i < cols; i++){
+        for(var j = 0; j < rows; j++){
+            maze[i][j] = Math.random() < density ? 0 : 1;
+    
+            drawBlock(i, j, maze[i][j]);
+        }
+    }
+    document.getElementById("btnCreateMaze").removeAttribute("disabled");
+}
+function createCanvas() {
+    var canvas = document.createElement( "canvas" );
+    wid = document.getElementById("maze").offsetWidth - padding; 
+    wid = 900;
+    hei = 400;
+    
+    canvas.width = wid; canvas.height = hei;
+    canvas.id = "canvas";
+    ctx = canvas.getContext( "2d" );
+    ctx.fillStyle = "gray"; ctx.fillRect( 0, 0, wid, hei );
+    var div = document.getElementById("maze");
+    div.appendChild( canvas ); 
+}
+
+function init() {
+    createCanvas();
+}
+
+function onCreate() {
+
+    document.getElementById("btnCreateMaze").setAttribute("disabled", "disabled");
+
+    wid = document.getElementById("maze").offsetWidth - padding; 
+    wid=900;
+    hei = 400;
+
+    cols = eval(document.getElementById("cols").value); 
+    rows = eval(document.getElementById("rows").value);
+
+    var mazeType = document.getElementById("sltType").value;
+
+    if(mazeType == "Maze1") {
+        cols = cols + 1 - cols % 2;
+        rows = rows + 1 - rows % 2;    
+    }
+
+    maze = createArray( cols, rows );
+
+    var canvas = document.getElementById("canvas");
+    canvas.width = wid;
+    canvas.height = hei;
+    s = canvas.width / (grid * cols);
+    canvas.height = s * grid * rows;
+
+    ctx.scale(s, s);
+
+ 
+    if(mazeType == "Maze1") {
+
+        start.x = Math.floor( Math.random() * ( cols / 2 ) );
+        start.y = Math.floor( Math.random() * ( rows / 2 ) );
+        if( !( start.x & 1 ) ) start.x++; if( !( start.y & 1 ) ) start.y++;
+        maze[start.x][start.y] = 0;
+
+        if(document.getElementById("chkAnimated").checked) {
+
+            createMaze1();
+        }
+        else {
+
+            createMaze1NonAni();
+        }
+    }
+    else {
+
+        density = document.getElementById("density").value / 100;
+        start.x = 0;
+        start.x = 0;
+
+        if(document.getElementById("chkAnimated").checked) {
+
+            createMaze2();
+        }
+        else {
+
+            createMaze2NonAni();
+        }
+    }
+}
+
+function onSltType() {
+    if(document.getElementById("sltType").value == "Maze2") {
+        document.getElementById("density").removeAttribute("disabled");
+    }
+    else {
+        document.getElementById("density").setAttribute("disabled", "disabled");
+    }
+}
