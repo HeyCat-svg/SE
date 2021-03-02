30996bbde78789f6865c7f76e8fc6583625a9979
Kirino Kousaka <1345226109@qq.com>
2021-03-02 10:19:56 +0800

    del: fxmark

--- CSE labs/lab4/fxmark/src/cpupol.h
+++
@@ -1,9 +0,0 @@
-const char *MODEL_NAME                             = "Intel(R) Core(TM) i7-8750H CPU @ 2.20GHz";
-const unsigned int PHYSICAL_CHIPS                  = 1;
-const unsigned int CORE_PER_CHIP                   = 1;
-const unsigned int SMT_LEVEL                       = 1;
-const unsigned int CACHE_PER_CORE                  = 9216;
-const unsigned int seq_cores[]                     = {0};
-const unsigned int rr_cores[]                      = {0};
-const unsigned int test_hw_thr_cnts_fine_grain[]   = {1};
-const unsigned int test_hw_thr_cnts_coarse_grain[] = {1};
